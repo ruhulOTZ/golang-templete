2fa6837d127b701ef2516efb179b6e13e3264aa2
riff3r <arif4alll@gmail.com>
2025-08-31 09:23:06 +0600

    feat: enhance configuration management with database settings and add docker-compose for PostgreSQL

--- config/config.go
+++ config/config.go
@@ -14,6 +14,11 @@ type Config struct {
 	Version     string
 	ServiceName string
 	HttpPort    int
+	DBName      string
+	DBHost      string
+	DBUser      string
+	DBPassword  string
+	DBPort      int
 }
 
 func loadConfig() Config {
@@ -26,15 +31,22 @@ func loadConfig() Config {
 	version := os.Getenv("VERSION")
 	serviceName := os.Getenv("SERVICE_NAME")
 	httpPort := os.Getenv("HTTP_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbPortStr := os.Getenv("DB_PORT")
 
-	if version == "" || serviceName == "" || httpPort == "" {
-		fmt.Println("Missing required environment variables")
+	port, err := strconv.ParseInt(httpPort, 10, 64)
+	dbPort, dbPortErr := strconv.ParseInt(dbPortStr, 10, 64)
+
+	if err != nil || dbPortErr != nil {
+		fmt.Println("Port must be a number")
 		os.Exit(1)
 	}
 
-	port, err := strconv.ParseInt(httpPort, 10, 64)
-	if err != nil {
-		fmt.Println("Port must be a number")
+	if version == "" || serviceName == "" || httpPort == "" || dbName == "" || dbHost == "" || dbUser == "" || dbPassword == "" || dbPort == 0 {
+		fmt.Println("Missing required environment variables")
 		os.Exit(1)
 	}
 
@@ -42,9 +54,19 @@ func loadConfig() Config {
 		Version:     version,
 		ServiceName: serviceName,
 		HttpPort:    int(port),
+		DBName:      dbName,
+		DBHost:      dbHost,
+		DBUser:      dbUser,
+		DBPassword:  dbPassword,
+		DBPort:      int(dbPort),
 	}
 
 	return configurations
+}
+
+func (c *Config) DatabaseUrl() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d ?sslmode=disable",
+		c.DBHost, c.DBUser, c.DBPassword, c.DBName, c.DBPort)
 }
 
 func GetConfig() Config {
